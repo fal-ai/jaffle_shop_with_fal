253027574f0841ac66175658256b613f9b4c602b
mederka <meder@kamalov.net>
2022-10-04 17:19:50 +0000

    PR comments

--- models/staging/raw_customers.py
+++ models/staging/raw_customers.py
@@ -3,8 +3,7 @@ from sshfs import SSHFileSystem
 
 
 def model(dbt, session) -> pd.DataFrame:
-    dbt.config(materialized="table")
-    dbt.config(fal_environment="sftp")
+    dbt.config(materialized="table", fal_environment="sftp")
 
     hostname = "35.193.199.9"
 
