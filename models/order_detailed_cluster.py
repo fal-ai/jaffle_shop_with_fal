a9c91e35e4c0f05bd857473ad18865c3a993768e
Burkay Gur <burkaygur@gmail.com>
2022-11-15 11:56:57 -0500

    Add clustering example

---
+++ models/order_detailed_cluster.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from kmodes.kmodes import KModes
+
+
+def model(dbt, fal):
+    df: pd.DataFrame = dbt.ref("order_detailed")
+    df_train = df[["size", "is_vegan", "is_vegetarian", "is_keto", "shape"]]
+
+    km_2 = KModes(n_clusters=3, init="Huang")
+    km_2.fit_predict(df_train)
+    df["cluster_label"] = km_2.labels_
+
+    # Hack: postgres adapter doesn't handle dates very well
+    df["order_date"] = df["order_date"].astype(str)
+
+    return df
